c38808c26fd0e23876ded4772b4357a5de61c03f
Malachi Gray <graymd.cfi@gmail.com>
2015-03-27 04:38:22 -0400

    fix hide edit and remove bli for admin only.  fix current user on front page and remove signed in/signed out tag

--- spec/models/item_complete_spec.rb
+++ spec/models/item_complete_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ItemComplete, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+
+  it { should belong_to(:bucket_list_item) }  
 end
